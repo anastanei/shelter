b14911225b7214575576b5cee464093401316216
Anastasiia Nikonova <anastaney@gmail.com>
2024-08-08 14:22:20 +0200

    fix: remove alt for decorative imgs

--- shelter/eslint.config.mjs
+++ shelter/eslint.config.mjs
@@ -5,4 +5,9 @@ import pluginJs from "@eslint/js";
 export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
+  {
+    rules: {
+      "jsx-a11y/alt-text": 0,
+    },
+  },
 ];

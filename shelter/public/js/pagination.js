ec6463a06ca174a5d0d8f6740f09bf74e5951c15
Anastasiia Nikonova <anastaney@gmail.com>
2024-09-02 12:30:35 +0200

    refactor: fix pagination structure

--- shelter/public/js/pagination.js
+++ shelter/public/js/pagination.js
@@ -1,17 +1,10 @@
 import card from "./card.js";
+import { shuffle } from "./scripts.js";
 
-const array = [...Array(8).keys()];;
-
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i-=1) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]]
-  }
-  return array;
-}
-
-function fullArray(array) {
+function fullArray() {
+  const array = [...Array(8).keys()];;
   const res = [];
+
   const getIndexes = (srcLength, resLength) => shuffle([...Array(srcLength).keys()]).slice(0, resLength);
   const shuffleAndPush = (arr) => res.push(...shuffle(arr));
 
@@ -30,4 +23,4 @@ function fullArray(array) {
   return [...res, ...res];
 }
 
-fullArray(array).map(index => card('[data-pagination]', index));
+fullArray().map(index => card('[data-pagination]', index));

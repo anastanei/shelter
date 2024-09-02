1f8cd9135d0ea9651a7ada24ec6be0b9d59d9575
Anastasiia Nikonova <anastaney@gmail.com>
2024-09-02 03:02:50 +0200

    feat: add pagination massive generation

--- shelter/public/js/pets-page.js
+++ shelter/public/js/pets-page.js
@@ -1,8 +1,34 @@
 import card from "./card/card.js";
 
+const array = [...Array(8).keys()];;
 
-for (let k = 0; k < 6; k++) {
-  for (let i = 0; i < 8; i++) {
-    card('[data-pagination]', i);
-  };
-};
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i-=1) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]
+  }
+  return array;
+}
+
+function fullArray(array) {
+  const res = [];
+  const getIndexes = (srcLength, resLength) => shuffle([...Array(srcLength).keys()]).slice(0, resLength);
+  const shuffleAndPush = (arr) => res.push(...shuffle(arr));
+
+  shuffleAndPush(array);
+
+  const indexes = getIndexes(6, 4);
+  indexes.forEach(index => res.push(array[index]));
+
+  const chunk1 = [];
+  indexes.sort((a, b) => b - a).forEach(index => chunk1.push(array.splice(index, 1)[0]));
+
+  shuffleAndPush(array);
+  shuffleAndPush(chunk1);
+  shuffleAndPush(array);
+
+  console.log(array, res, indexes, chunk1);
+  return [...res, ...res];
+}
+
+fullArray(array).map(index => card('[data-pagination]', index));

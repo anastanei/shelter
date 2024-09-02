40b457e59d8270ade6d069bd0dc58b7e2b87ab54
Anastasiia Nikonova <anastaney@gmail.com>
2024-09-02 10:45:40 +0200

    refactor: format fullArray function

--- shelter/public/js/pets-page.js
+++ shelter/public/js/pets-page.js
@@ -20,7 +20,9 @@ function fullArray(array) {
   const indexes = getIndexes(6, 4);
   indexes.forEach(index => res.push(array[index]));
 
-  const chunk = indexes.sort((a, b) => b - a).map(index => array.splice(index, 1)[0]);
+  const chunk = indexes.sort((a, b) => b - a)
+                       .map(index => array
+                       .splice(index, 1)[0]);
 
   shuffleAndPush(array);
   shuffleAndPush(chunk);
